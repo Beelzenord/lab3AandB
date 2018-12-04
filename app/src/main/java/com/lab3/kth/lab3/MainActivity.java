674d89174d3f692b139875e0bdd5f43c1314d896
nordpool <beelzenord@gmail.com>
2018-12-04 11:46:27 +0100

    init

--- app/src/main/java/com/lab3/kth/lab3/MainActivity.java
+++ app/src/main/java/com/lab3/kth/lab3/MainActivity.java
@@ -10,19 +10,58 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
 
     private float prevAngle = 0;
+    private float prevxFiltered = 0;
+    private float prevyFiltered = 0;
+    private float prevzFiltered = 0;
     private float acceAlpha = (float)0.85;
+    private float highPassFilter = (float) 0.12;
     private float accelPrevTimestamp = 0;
+    private ArrayList<Float> xValues;
+    private ArrayList<Float> yValues;
+    private ArrayList<Float> zValues;
+
     private TextView degreesView;
     SensorManager manager;
+
+    private TimerTask timerTask;
+
+    private final float THRESHOLD = 15;
+
+    private final long DURANTION = 1000L;
+
+    private boolean xDeviation;
+    private boolean yDeviation;
+    private boolean zDeviation;
+
+    private boolean colorFlag;
+
+    private Timer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        colorFlag = false;
+
+
+        xDeviation = false;
+        xValues = new ArrayList<Float>();
+        yValues = new ArrayList<Float>();
+        zValues = new ArrayList<Float>();
         degreesView = findViewById(R.id.degreesView);
+
+
+
+        timer = new Timer();
+
+
 
         manager = (SensorManager)
                 getSystemService(Context.SENSOR_SERVICE);
@@ -30,18 +69,20 @@ public class MainActivity extends AppCompatActivity {
                 manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         if (accelerometer != null) {
             manager.registerListener(
-                    sensorEventListener, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+                    sensorEventListener, accelerometer, SensorManager.SENSOR_DELAY_GAME);
         }
 
 
         manager = (SensorManager)
                 getSystemService(Context.SENSOR_SERVICE);
         Sensor gyro =
-                manager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+                manager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
         if (gyro != null) {
             manager.registerListener(
                     sensorEventListener, gyro, SensorManager.SENSOR_DELAY_NORMAL);
         }
+
+
     }
 
     private final SensorEventListener sensorEventListener = new SensorEventListener() {
@@ -55,6 +96,9 @@ public class MainActivity extends AppCompatActivity {
                 case Sensor.TYPE_GYROSCOPE :
 //                    handleGyroEvent(event);
                     break;
+
+                    case Sensor.TYPE_LINEAR_ACCELERATION:
+                        System.out.println(" X : " + event.values[0] + " Y " + event.values[1] + " Z "  + event.values[2]);
             }
         }
         @Override
@@ -68,9 +112,31 @@ public class MainActivity extends AppCompatActivity {
         float z = event.values[2];
         long time = event.timestamp;
 
+        float[] g = new float[3];
+        g = event.values.clone();
+        float norm_Of_g = (float) Math.sqrt(g[0] * g[0] + g[1] * g[1] + g[2] * g[2]);
+
+
+        // Normalize the accelerometer vector
+        g[0] = g[0] / norm_Of_g;
+        g[1] = g[1] / norm_Of_g;
+        g[2] = g[2] / norm_Of_g;
+
+        int inclination = (int) Math.round(Math.toDegrees(Math.acos(g[2])));
+
+
+      //  System.out.println("inclination " +inclination);
         float angle = zAngleDegrees(x, y, z);
-        angle = (prevAngle * (1-acceAlpha)) + (angle * acceAlpha);
+
+
+
+        angle = (prevAngle * (acceAlpha)) + (angle * (1-acceAlpha));
         prevAngle = angle;
+
+        float xFiltered = (prevxFiltered * (highPassFilter)) + (x * (1-highPassFilter));
+        float yFiltered = (prevxFiltered * (highPassFilter)) + (y * (1-highPassFilter));
+        float zFiltered = (prevxFiltered * (highPassFilter)) + (z * (1-highPassFilter));
+
 
         // Bottom left
         if (y < 0 && angle > 0) {
@@ -88,12 +154,62 @@ public class MainActivity extends AppCompatActivity {
             angle = 270 + tmp;
         }
 
-        float timeDiffMilli = (time - accelPrevTimestamp) / 1000000;
-        if (timeDiffMilli > 1000) {
+        xValues.add(xFiltered);
+        yValues.add(yFiltered);
+        zValues.add(zFiltered);
+
+
+        if(xValues.size()==50 || yValues.size() == 50 || zValues.size() == 50){
+            xDeviation = findStandardDeviation(xValues);
+            yDeviation = findStandardDeviation(yValues);
+            zDeviation = findStandardDeviation(zValues);
+        }
+        if(xDeviation){
+
+            degreesView.setTextColor(getResources().getColor(R.color.red,null));
+        }
+        else{
+
+        }
+
+       // float timeDiffMilli = (time - accelPrevTimestamp) / 1000000;
+    //    if (timeDiffMilli > 1000) {
             accelPrevTimestamp = time;
             degreesView.setText(Math.round(angle) + "°");
 //            Log.i("Main", "angle; " + angle);
+     //   }*/
+    }
+
+    private boolean findStandardDeviation(ArrayList<Float> xValues) {
+          float variance = 0;
+          float sum = 0;
+
+
+          for(Float value : xValues){
+              float tmp = value;
+              sum += value;
+              tmp = tmp*tmp;
+
+              variance +=tmp;
+          }
+          variance/= xValues.size();
+         float standDev = (float) Math.sqrt(variance);
+         float average = sum / xValues.size();
+
+        Log.i("Mean","Standard Deviation : " + standDev);
+         xValues.remove(0);
+        Log.i("Mean","Mean : " + average);
+        xValues.remove(0);
+
+        if((average + THRESHOLD) < standDev){
+            System.out.println("Performing shift " + average + " " + standDev);
+            return true;
         }
+        else{
+
+            return false;
+        }
+
     }
 
     private float zAngleDegrees(float x, float y, float z) {
